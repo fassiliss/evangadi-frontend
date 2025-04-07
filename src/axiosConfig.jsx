216b1d529327c50aa4d9fe41a8b3d816ff62bb77
fassiliss <fassiliss@gmail.com>
2025-04-06 21:49:42 -0500

    thrid commit

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -3,6 +3,6 @@ import axios from "axios";
 const axiosBase = axios.create({
   // baseURL: "http://localhost:6063/api",
 
-  baseURL: "https://evangadi-forum-backend-nfee.onrender.com/api",
+  baseURL: "https://evangadi-backend-2-s5hi.onrender.com/api",
 });
 export default axiosBase;
